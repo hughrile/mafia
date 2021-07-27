40b4cbf4c3b14b8f385db5521f0a4a41c9202b68
Riley Hughes <hughrile@gmail.com>
2021-07-27 20:47:52 +0930

    Server changes

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get("/", function(req, res){
 	res.render("index");
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, "views")));
 
 var roomno = 1; // room variable
 
